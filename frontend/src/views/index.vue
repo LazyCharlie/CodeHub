f6e389d58dd9b1409c27f492be71478e4d60292f
Charlie <3376554599@qq.com>
2019-04-13 19:49:22 +0800

    登陆界面

--- frontend/src/views/index.vue
+++ frontend/src/views/index.vue
@@ -1,52 +1,71 @@
-<style scoped lang="less">
-    .index{
-        width: 100%;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        text-align: center;
-        h1{
-            height: 150px;
-            img{
-                height: 100%;
-            }
-        }
-        h2{
-            color: #666;
-            margin-bottom: 200px;
-            p{
-                margin: 0 0 50px;
-            }
-        }
-        .ivu-row-flex{
-            height: 100%;
-        }
-    }
+<style>
 </style>
 <template>
-    <div class="index">
-        <Row type="flex" justify="center" align="middle">
-            <Col span="24">
-                <h1>
-                    <img src="../images/logo.png">
-                </h1>
-                <h2>
-                    <p>Welcome to your iView app!</p>
-                    <Button @click="handleStart">Start iView</Button>
-                </h2>
-            </Col>
-        </Row>
+    <div>
+        <Button @click="modal2 = true">登录</Button>
+        <Modal
+                v-model="modal2"
+                footer-hide>
+            <p slot="header"
+               style="text-align:left;font-size:17px">
+                <span>登录</span>
+            </p>
+            <div style="text-align:center;margin-left:auto;margin-right:auto;">
+                <Form ref="formInline" :model="formInline" :rules="ruleInline" style="margin-top:20px">
+                    <p style="margin-left:-227px;font-weight:bold;font-size:14px">用户名 或 邮箱</p>
+                    <FormItem prop="user" style="margin-left:auto;margin-right:auto;width:320px">
+                        <Input type="text" v-model="formInline.user" placeholder="Username" style="font-size:24px;margin-left:auto;margin-right:auto;width:320px;margin-top:5px" size="large">
+                            <Icon type="ios-person-outline" slot="prepend"></Icon>
+                        </Input>
+                    </FormItem>
+                    <FormItem prop="password" style="margin-left:auto;margin-right:auto;width:320px">
+                        <p style="margin-left:-292px;margin-top:-7px;font-weight:bold;font-size:14px">密码</p>
+                        <Input type="password" v-model="formInline.password" placeholder="Password" style="margin-top:-2px" size="large">
+                            <Icon type="ios-lock-outline" slot="prepend"></Icon>
+                        </Input>
+                    </FormItem>
+                    <FormItem style="margin-top:-25px">
+                        <a href="forget" target="_blank">忘记密码?</a>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;&nbsp;
+                        尚未拥有账户？
+                        <a href="register" target="_blank">注册</a>
+                    </FormItem>
+                    <FormItem>
+                        <Button style="width:320px" type="primary" @click="handleSubmit('formInline')">登录</Button>
+                    </FormItem>
+                </Form>
+            </div>
+        </Modal>
     </div>
 </template>
 <script>
     export default {
+        data () {
+            return {
+                modal2: false,
+                formInline: {
+                    user: '',
+                    password: ''
+                },
+                ruleInline: {
+                    user: [
+                        { trigger: 'blur' }
+                    ],
+                    password: [
+                        { trigger: 'blur' },
+                    ]
+                }
+            }
+        },
         methods: {
-            handleStart () {
-                this.$Modal.info({
-                    title: 'Bravo',
-                    content: 'Now, enjoy the convenience of iView.'
-                });
+            handleSubmit(name) {
+                this.$refs[name].validate((valid) => {
+                    if (valid) {
+                        this.$Message.success('Success!');
+                    } else {
+                        this.$Message.error('Fail!');
+                    }
+                })
             }
         }
     }
